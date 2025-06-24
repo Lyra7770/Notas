6e83fa88f5233fade159c78f4c7705adf083a456
Lyra7770 <lyramir955@gmail.com>
2025-06-24 00:19:44 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 let texto = ""; // Lo que el usuario escribe
+let escribiendo = true;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let c = createCanvas(windowWidth, windowHeight);
+  c.canvas.addEventListener("mousedown", () => {
+    // Forzar el foco cuando el usuario haga clic en el canvas
+    let fs = fullscreen();
+    if (!fs && windowWidth < 600) fullscreen(true);
+    c.elt.focus();
+  });
+
   textFont('Handlee');
   textSize(28);
   fill(50, 90);
@@ -10,8 +18,7 @@ function setup() {
 
 function draw() {
   background(255, 250, 240); // Fondo cálido, tipo papel
-
-  drawLines(); // Líneas de cuaderno
+  drawLines();
 
   // Encabezado
   textAlign(LEFT, TOP);
@@ -26,21 +33,24 @@ function draw() {
   text(texto, 60, 60, width - 100, height - 100);
 }
 
-function keyTyped() {
-  if (key === 'Backspace') {
+function keyPressed() {
+  if (keyCode === BACKSPACE) {
     texto = texto.substring(0, max(0, texto.length - 1));
-  } else if (key !== CODED) {
+  }
+}
+
+function keyTyped() {
+  // Evita que se escriban teclas especiales
+  if (key.length === 1) {
     texto += key;
   }
 }
 
 function drawLines() {
-  // Línea de margen rosado (como en cuaderno)
-  stroke(255, 160, 180, 150); // rosado suave
+  stroke(255, 160, 180, 150); // margen rosado
   strokeWeight(2);
   line(50, 0, 50, height);
 
-  // Líneas horizontales tipo cuaderno
   stroke(220, 180);
   strokeWeight(1);
   for (let y = 60; y < height; y += 40) {
